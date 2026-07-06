a4561b57b1181f880cfc940c2d3e7002217ee4d9
André Brandão Ghira Marques <127451848+andre-marques-a22207598@users.noreply.github.com>
2026-07-06 04:16:49 +0000

    faltam estes ficheiros

--- portofolio/views.py
+++ portofolio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-
+from .forms import ProjetoForm, CompetenciaForm, TecnologiaForm, FormacaoForm
 from .models import (
     Owner,
     Universidade,
@@ -10,21 +10,19 @@ from .models import (
     UnidadeCurricular,
     Projeto,
     Tecnologia,
+    Competencia,
     MakingOf
 )
-
 
 # 🔹 INÍCIO
 def inicio_view(request):
     return render(request, 'portofolio/base.html')
-
 
 # 🔹 OWNER
 def owner_detail(request):
     owner = Owner.objects.first()
 
     return render(request, 'portofolio/owner.html', {'owner': owner})
-
 
 # 🔹 SOBRE
 def sobre_view(request):
@@ -39,33 +37,6 @@ def licenciatura_detail(request):
     return render(request, 'portofolio/licenciatura.html', {
         'licenciatura': licenciatura
     })
-
-
-# 🔹 FORMAÇÃO
-def formacao_list(request):
-    formacoes = Formacao.objects.select_related(
-        'owner'
-    ).all()
-    return render(request, 'portofolio/formacoes.html', {
-        'formacoes': formacoes
-    })
-
-
-def formacao_detail(request, id):
-    formacao = get_object_or_404(Formacao, id=id)
-
-    return render(request, 'portofolio/formacao_detail.html', {
-        'formacao': formacao
-    })
-
-
-def apaga_formacao_view(request, formacao_id):
-    formacao = get_object_or_404(Formacao, id=formacao_id)
-
-    formacao.delete()
-
-    return redirect('formacao_list')
-
 
 # 🔹 DOCENTES
 def docente_list(request):
@@ -118,9 +89,8 @@ def uc_detail(request, id):
 
 
 # 🔹 PROJETOS
-def projeto_list(request):
+def projetos(request):
     projetos = Projeto.objects.select_related(
-        'owner',
         'unidade_curricular'
     ).prefetch_related(
         'tecnologias'
@@ -130,11 +100,30 @@ def projeto_list(request):
         'projetos': projetos
     })
 
+def edita_projeto_view(request, projeto_id):
+    projeto = get_object_or_404(Projeto, id=projeto_id)
+
+    if request.method == "POST":
+        form = ProjetoForm(request.POST, request.FILES, instance=projeto)
+
+        if form.is_valid():
+            form.save()
+            return redirect("projetos")
+    else:
+        form = ProjetoForm(instance=projeto)
+
+    return render(
+        request,
+        "portofolio/edita_projeto.html",
+        {
+            "form": form,
+            "projeto": projeto,
+        },
+    )
 
 def projeto_detail(request, id):
     projeto = get_object_or_404(
         Projeto.objects.select_related(
-            'owner',
             'unidade_curricular'
         ).prefetch_related(
             'tecnologias'
@@ -154,24 +143,57 @@ def novo_projeto_view(request):
         return redirect('projetos')
     return render(request, 'portofolio/novo_projeto.html', {'form': form})
 
-
-def edita_projeto_view(request, projeto_id):
-    projeto = get_object_or_404(Projeto, id=projeto_id)
-
-    form = ProjetoForm(request.POST or None, request.FILES, instance=projeto)
-
-    if form.is_valid():
-        form.save()
-        return redirect('projetos')
-
-    return render(request, 'portofolio/edita_projeto.html', {'form': form, 'projeto': projeto})
-
-
 def apaga_projeto_view(request, projeto_id):
     projeto = get_object_or_404(Projeto, id=projeto_id)
     projeto.delete()
     return redirect('projetos')
 
+
+# 🔹 Competencias
+
+def competencias_view(request):
+    competencias = Competencia.objects.all()
+    return render(request, 'portofolio/competencias.html', {
+    'competencias': competencias,
+    #'is_gestor': gestor_portofolio(request.user)
+    })
+
+def nova_competencia_view(request):
+    form = CompetenciaForm(request.POST or None, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('competencias')
+    return render(request, 'portofolio/nova_competencia.html', {'form': form})
+
+def edita_competencia_view(request, competencia_id):
+    competencia = get_object_or_404(Competencia, id=competencia_id)
+
+    if request.method == "POST":
+        form = CompetenciaForm(
+            request.POST,
+            request.FILES,
+            instance=competencia
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect("competencias")
+    else:
+        form = CompetenciaForm(instance=competencia)
+
+    return render(
+        request,
+        "portofolio/edita_competencia.html",
+        {
+            "form": form,
+            "competencia": competencia,
+        },
+    )
+
+def apaga_competencia_view(request, tecnologia_id):
+    competencia = get_object_or_404(Competencia, id=tecnologia_id)
+    competencia.delete()
+    return redirect('competencias')
 
 # 🔹 TECNOLOGIAS
 
@@ -191,16 +213,89 @@ def nova_tecnologia_view(request):
 
 def edita_tecnologia_view(request, tecnologia_id):
     tecnologia = get_object_or_404(Tecnologia, id=tecnologia_id)
-    form = TecnologiaForm(request.POST or None, request.FILES, instance=tecnologia)
 
-    if form.is_valid():
-        form.save()
-        return redirect('tecnologias')
+    if request.method == "POST":
+        form = TecnologiaForm(
+            request.POST,
+            request.FILES,
+            instance=tecnologia
+        )
 
-    return render(request, 'portofolio/edita_tecnologia.html', {'form': form})
+        if form.is_valid():
+            form.save()
+            return redirect("tecnologias")
+    else:
+        form = TecnologiaForm(instance=tecnologia)
 
+    return render(
+        request,
+        "portofolio/edita_tecnologia.html",
+        {
+            "form": form,
+            "tecnologia": tecnologia,
+        },
+    )
 
 def apaga_tecnologia_view(request, tecnologia_id):
     tecnologia = get_object_or_404(Tecnologia, id=tecnologia_id)
     tecnologia.delete()
     return redirect('tecnologias')
+
+
+# 🔹 Formações
+
+def formacoes_view(request):
+    formacoes = Formacao.objects.all()
+    return render(request, 'portofolio/formacoes.html', {
+    'formacoes': formacoes,
+    #'is_gestor': gestor_portofolio(request.user)
+    })
+
+def nova_formacao_view(request):
+    form = FormacaoForm(request.POST or None, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('formacoes')
+    return render(request, 'portofolio/nova_formacao.html', {'form': form}) 
+
+
+def apaga_formacao_view(request, formacao_id):
+    formacao = get_object_or_404(Formacao, id=formacao_id)
+
+    if request.method == "POST":
+        formacao.delete()
+        return redirect("formacoes")
+
+    return render(
+        request,
+        "portofolio/apaga_formacao.html",
+        {
+            "formacao": formacao,
+        },
+    )
+
+
+def edita_formacao_view(request, formacao_id):
+    formacao = get_object_or_404(Formacao, id=formacao_id)
+
+    if request.method == "POST":
+        form = FormacaoForm(
+            request.POST,
+            request.FILES,
+            instance=formacao
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect("formacoes")
+    else:
+        form = FormacaoForm(instance=formacao)
+
+    return render(
+        request,
+        "portofolio/edita_formacao.html",
+        {
+            "form": form,
+            "formacao": formacao,
+        },
+    )
